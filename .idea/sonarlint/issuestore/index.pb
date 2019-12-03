d8718aee000e79c4df688c3e7ab4ec012bb048bd
altin <altincampbell121690@gmail.com>
2019-12-02 21:03:13 -0800

    all

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,21 @@ A
 public/index.html,f\4\f43ecdf8d666ee908aacce037b4cd72159e4741b
 F
 src/components/seat.js,7\a\7aaf6c5601bf2edc2f67a761db8fe4d3fc62a9ec
+\
+,.idea/inspectionProfiles/Project_Default.xml,4\9\496a238a6afa168dbaf6efd37bb459331589579c
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+A
+package-lock.json,f\a\fa288d1472d29beccb489a676f68739ad365fc47
+D
+public/manifest.json,4\2\42e89cd2213b9d5dfae4f63522702c623170d792
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+C
+.vscode/launch.json,f\1\f14cb12fa7462cdba1e6e6e4b499bce722fe22ba
+K
+scripts/event-hub-reader.js,f\b\fb699d5671cb9fec4f7651bc843d0763934088e5
+9
+	server.js,d\c\dc60ea9ee1ff6246bf342463277d5b914db2883d
+N
+public/js/chart-device-data.js,4\0\409a216bce508abb34aa016584d4f8a15506dd7b
--- src/components/seat.js
+++ src/components/seat.js
@@ -1,4 +1,12 @@
 import React, { Component } from "react";
+import Websocket from "react-websocket";
+/* eslint-disable no-restricted-globals */
+
+const protocol = document.location.protocol.startsWith("https")
+  ? "wss://"
+  : "ws://";
+console.log(protocol);
+console.log(location.host);
 
 class Seat extends Component {
   constructor() {
@@ -9,6 +17,21 @@ class Seat extends Component {
       isTaken: false,
       color: "red"
     };
+  }
+
+  handleData(data) {
+    let result = JSON.parse(data);
+    console.log(result.IotData);
+    //let isTaken = result.IotData.TEST;
+    let newColor;
+    if (result.IotData.TEST) {
+      newColor = "red";
+      // isTaken = false;
+    } else {
+      newColor = "green";
+      // isTaken = true;
+    }
+    this.setState({ color: newColor, isTaken: result.IotData.TEST });
   }
 
   handleChange = event => {
@@ -28,13 +51,16 @@ class Seat extends Component {
     return (
       <div className="Seating">
         <div className="class-Room">
+          <Websocket
+            url={protocol + location.host}
+            onMessage={this.handleData.bind(this)}
+          />
           <div
             className="circle"
             style={{ background: this.state.color }}
           ></div>
         </div>
         <button className="buttons" onClick={this.handleChange}>
-          {" "}
           refresh
         </button>
       </div>
